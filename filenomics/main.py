6df88375f839d9a0723215cc8d95f2bbe4f4cbb7
Ian Andwati <andwatiian@gmail.com>
2023-10-05 00:42:27 +0300

    hmm

--- filenomics/main.py
+++ filenomics/main.py
@@ -1,14 +1,35 @@
 import os
+from pathlib import Path
 
-from flask import Flask, flash, redirect, render_template, request, url_for
+from dotenv import load_dotenv
+from flask import (
+    Flask,
+    flash,
+    redirect,
+    render_template,
+    request,
+    send_from_directory,
+    url_for,
+)
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = "uploads"
+load_dotenv()
+
+# BASE_DIR = Path.parent
+
+# if os.path.exists(BASE_DIR  "uploads"):
+#     UPLOAD_FOLDER = "uploads"
+# else:
+#     UPLOAD_FOLDER = os.mkdir("uploads")
+
+
 ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg"}
+
 
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 app.config["MAX_CONTENT_LENGTH"] = 10 * 1000 * 1000
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
 
 def allowed_file(filename):
@@ -18,18 +39,24 @@ def allowed_file(filename):
 @app.route("/", methods=["GET", "POST"])
 def upload_file():
     if request.method == "POST":
+        # check if the post request has the file part
         if "file" not in request.files:
             flash("No file part")
             return redirect(request.url)
         file = request.files["file"]
-
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
         if file.filename == "":
             flash("No selected file")
             return redirect(request.url)
-
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
-            flash("Upload succesful")
-            return render_template("index.html")
+            return redirect(url_for("download_file", name=filename))
+
     return render_template("index.html")
+
+
+@app.route("/uploads/<name>")
+def download_file(name):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
