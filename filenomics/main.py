eb5d1d91f97a8626e36bb3fbefce7ed93fe84f0e
Ian Andwati <andwatiian@gmail.com>
2023-10-04 23:57:32 +0300

    added upload file method

--- filenomics/main.py
+++ filenomics/main.py
@@ -1,8 +1,35 @@
-from flask import Flask, render_template
+import os
+
+from flask import Flask, flash, redirect, render_template, request, url_for
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = "uploads"
+ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg"}
 
 app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+app.config["MAX_CONTENT_LENGTH"] = 10 * 1000 * 1000
 
 
-@app.route("/")
-def index():
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route("/", methods=["GET", "POST"])
+def upload_file():
+    if request.method == "POST":
+        if "file" not in request.files:
+            flash("No file part")
+            return redirect(request.url)
+        file = request.files["file"]
+
+        if file.filename == "":
+            flash("No selected file")
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+            flash("Upload succesful")
+            return render_template("index.html")
     return render_template("index.html")
